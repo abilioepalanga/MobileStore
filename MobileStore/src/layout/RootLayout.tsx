aa8530f0610824fbf6abd325606e04ff9cf5c339
abilioepalanga <up202300492@edu.fe.up.pt>
2026-02-18 00:27:23 +0000

    Create RootLayout file

---
+++ MobileStore/src/layout/RootLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const RootLayout = () => {
+    return (
+        <div>
+            <div>Navbar</div>
+            <div>Main</div>
+            <div>Footer</div>
+        </div>
+    );
+};
--- MobileStore/src/router/index.tsx
+++ MobileStore/src/router/index.tsx
@@ -1,8 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "../layout/RootLayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <p>Início</p>,
+        element: <RootLayout />,
+        children: [
+            {
+                path: "",
+                element: <div>Inicio</div>,
+            },
+            {
+                path: "celulares",
+                element: <div>Celulares</div>,
+            },
+            {
+                path: "sobre",
+                element: <div>Sobre Nos</div>,
+            },
+        ],
     },
 ]);
