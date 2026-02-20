a6b9205017608f9e28727c05995960373de0b4db
abilioepalanga <up202300492@edu.fe.up.pt>
2026-02-20 01:12:25 +0000

    Start to create the footer file

--- MobileStore/src/router/index.tsx
+++ MobileStore/src/router/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RootLayout } from "../layout/RootLayout";
-import { Navbar } from "../shared/Navbar";
 
 export const router = createBrowserRouter([
     {
